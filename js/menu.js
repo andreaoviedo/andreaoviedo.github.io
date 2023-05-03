6502ee67f8f45a7f7c07051b998fc9c55395db59
Andrea Oviedo <andreaov_21@hotmail.com>
2023-05-03 01:24:20 +0000

    cambio menú

--- js/menu.js
+++ js/menu.js
@@ -18,8 +18,8 @@ document.write(`<nav class="navbar navbar-expand-lg">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
    
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="../../CV"
-          >Curriculum</a
+        <a class="nav-link active" aria-current="page" href="../../index2.html#about"
+          >Acerca de mí</a
         >
       </li>
       <li class="nav-item dropdown">
