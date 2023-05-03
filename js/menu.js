5af0d140dc727d716b2014cade49df0854cbe73a
Andrea Oviedo <andreaov_21@hotmail.com>
2023-05-03 04:46:30 +0000

    cambios

--- js/menu.js
+++ js/menu.js
@@ -18,7 +18,7 @@ document.write(`<nav class="navbar navbar-expand-lg">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
    
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="../../index2.html#about"
+        <a class="nav-link active" aria-current="page" href="../../index.html#about"
           >Acerca de mí</a
         >
       </li>
@@ -47,7 +47,7 @@ document.write(`<nav class="navbar navbar-expand-lg">
         </ul>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="../../index.html"
+        <a class="nav-link active" aria-current="page" href="../../index.html#contacto"
           >Contacto</a
         >
       </li>
