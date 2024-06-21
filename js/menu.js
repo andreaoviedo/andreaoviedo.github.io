e67f45f323fe336690797e1ae8e0d6e5f0338b91
Andrea Oviedo <andreaoviedov@gmail.com>
2024-06-21 17:53:08 -0600

    menu

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,8 @@ document.write(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="container-fluid">
   <a class="navbar-brand" style="color:#ff9c9c" href="../../"
     >
-    <img class="img-fluid" style="max-width:300px;" src="../img/andreaoviedo.jpg" alt="Andrea Oviedo">
-    <span style="font-weight:700">andrea</span>oviedo.</a
-  >
+    <img class="img-fluid" style="max-width:80px;" src="../img/andreaoviedo-logo.png" alt="Andrea Oviedo">
+   
   <button
     class="navbar-toggler"
     type="button"
